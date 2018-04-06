113230c33c657cc9299d41829c003e9ce0b54997
Hernan Herrera Bruna <wech_chile_mac@Air-de-Hernan.lan>
2018-04-06 14:43:19 -0300

    se activa envento de

---
+++ js/app.js
@@ -0,0 +1,47 @@
+window.onload= function(){
+   document.getElementById('1').onclick= function(){
+     alert('uno');
+     document.getElementById('display').value='1';
+   }
+   document.getElementById('2').onclick= function(){
+     alert('uno');
+     document.getElementById('display').value='1';
+   }
+   document.getElementById('3').onclick= function(){
+     alert('uno');
+     document.getElementById('display').value='1';
+   }
+   document.getElementById('4').onclick= function(){
+     alert('uno');
+     document.getElementById('display').value='1';
+   }
+   document.getElementById('5').onclick= function(){
+     alert('uno');
+     document.getElementById('display').value='1';
+   }
+   document.getElementById('6').onclick= function(){
+     alert('uno');
+     document.getElementById('display').value='1';
+   }
+   document.getElementById('7').onclick= function(){
+     alert('uno');
+     document.getElementById('display').value='1';
+   }
+   document.getElementById('8').onclick= function(){
+     alert('uno');
+     document.getElementById('display').value='1';
+   }
+   document.getElementById('9').onclick= function(){
+     alert('uno');
+     document.getElementById('display').value='1';
+   }
+   document.getElementById('0').onclick= function(){
+     alert('uno');
+     document.getElementById('display').value='1';
+   }
+   document.getElementById('.').onclick= function(){
+     alert('uno');
+     document.getElementById('display').value='1';
+   }
+
+}
