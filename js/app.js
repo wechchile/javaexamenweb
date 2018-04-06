3f45265e38e1fa245122732a7bb1ad44578b74f9
wech <wech@tie.cl>
2018-04-06 16:24:14 -0300

    se ingresa los numero a la pantalla

--- js/app.js
+++ js/app.js
@@ -1,45 +1,47 @@
 window.onload= function(){
+  var gvalor=0
   function pantalla(valor){
     var  val1= document.getElementById('display').innerHTML;
+    var  valor2=parseInt(val1);
     console.log(val1);
-    console.log(parseInt(val1));
-    if (val1='0'){
+    if (val1=='0'){
       document.getElementById('display').innerHTML=valor;
-    }else if (parseInt(val1)>0) {
+    }else if (valor2>0) {
       document.getElementById('display').innerHTML=val1.concat(valor);
     }
-    console.console.log(parseInt(val1)+valor);
+    console.log(valor2+valor);
   }
    document.getElementById('1').onclick= function(){
-     pantalla('1');
+     pantalla(1);
    }
    document.getElementById('2').onclick= function(){
-     document.getElementById('display').innerHTML='2';
+     pantalla(2);
    }
    document.getElementById('3').onclick= function(){
-     document.getElementById('display').innerHTML='3';
+     pantalla(3);
    }
    document.getElementById('4').onclick= function(){
-     document.getElementById('display').innerHTML='4';
+     pantalla(4);
    }
    document.getElementById('5').onclick= function(){
-     document.getElementById('display').innerHTML='5';
+     pantalla(5);
    }
    document.getElementById('6').onclick= function(){
-     document.getElementById('display').innerHTML='6';
+     pantalla(6);
    }
    document.getElementById('7').onclick= function(){
-     document.getElementById('display').innerHTML='7';
+     pantalla(7);
    }
    document.getElementById('8').onclick= function(){
-     document.getElementById('display').innerHTML='8';
+     pantalla(8);
    }
    document.getElementById('9').onclick= function(){
-     document.getElementById('display').innerHTML='9';
+     pantalla(9);
    }
    document.getElementById('0').onclick= function(){
-     document.getElementById('display').innerHTML='0';
+     pantalla(0);
    }
+
    document.getElementById('punto').onclick= function(){
      document.getElementById('display').value='.';
    }
