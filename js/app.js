6ae5492fcefa93d58d83e1911ac18d4c699fc35c
wech <wech@tie.cl>
2018-04-21 14:15:49 -0300

    con la operaciones en funcionamiento

--- js/app.js
+++ js/app.js
@@ -1,10 +1,68 @@
 window.onload= function(){
   var bandera=0
+  var operacion='';
+  var memoria=0;
+
+  function sumar(valor){
+      memoria=parseFloat(valor);
+      operacion ='sumar';
+      console.log(operacion);
+      console.log(memoria);
+
+  }
+
+  function resta(valor){
+        memoria=parseFloat(valor);
+      operacion ='resta';
+      console.log(operacion);
+      console.log(memoria);
+
+  }
+
+   function multipli(valor){
+        memoria=parseFloat(valor);
+      operacion ='multp';
+      console.log(operacion);
+      console.log(memoria);
+   }
+
+   function dividir(valor){
+        memoria=parseFloat(valor);
+      operacion ='divid';
+      console.log(operacion);
+      console.log(memoria);
+   }
+
+   function camsign(valor){
+      var resul= parseFloat(valor);
+      resul=resul*-1;
+      document.getElementById('display').innerHTML=resul; 
+   }
+
+  function resultado(valor){
+      var resul =0;
+      if (operacion=='sumar'){
+         resul= memoria + parseFloat(valor); 
+      }
+      if (operacion=='resta'){
+         resul= memoria - parseFloat(valor); 
+      }
+      if (operacion=='divid'){
+         resul= memoria / parseFloat(valor); 
+      }
+      if (operacion=='multp'){
+         resul= memoria * parseFloat(valor); 
+      }
+
+      document.getElementById('display').innerHTML=resul;
+  }
+  
+
   function pantalla(valor){
     var  val1= document.getElementById('display').innerHTML;
     var  valor2=parseFloat(val1);
     console.log(val1);
-    if (val1=='0'){
+    /* if (val1=='0'){
        if (valor=='.'){
          if (bandera==0){
            document.getElementById('display').innerHTML=val1.concat(valor);
@@ -24,7 +82,8 @@ window.onload= function(){
             document.getElementById('display').innerHTML=val1.concat(valor);
        }
       }
-    }
+    }*/
+     document.getElementById('display').innerHTML=valor;
   }
 
    document.getElementById('1').onclick= function(){
@@ -59,13 +118,34 @@ window.onload= function(){
    document.getElementById('0').onclick= function(){
      pantalla(0);
    }
+   //inicio de operacion matematica
+   document.getElementById('mas').onclick= function(){
+     sumar(document.getElementById('display').innerHTML);
+   }
+   document.getElementById('igual').onclick= function(){
+     resultado(document.getElementById('display').innerHTML);
+   }
+   document.getElementById('menos').onclick= function(){
+     resta(document.getElementById('display').innerHTML);
+   }
+   document.getElementById('dividido').onclick= function(){
+     dividir(document.getElementById('display').innerHTML);
+   }
+   document.getElementById('por').onclick= function(){
+     multipli(document.getElementById('display').innerHTML);
+   }
+   document.getElementById('sign').onclick= function(){
+     camsign(document.getElementById('display').innerHTML);
+   }
 
    document.getElementById('punto').onclick= function(){
     pantalla('.')
    }
+
    document.getElementById('on').onclick= function(){
     document.getElementById('display').innerHTML='0';
     bandera=0;
+    operacion='';
     }
 
 
